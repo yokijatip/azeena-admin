2613e0785b2e5c80600605fd128bb47b745e1a9f
Yoki Jati Perkasa <yokijati@gmail.com>
2026-02-06 01:17:03 +0700

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
